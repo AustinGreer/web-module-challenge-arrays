8aed15951dc3b76be069aae17fcf242709a8539f
Austin Greer <agreer.ag12@gmail.com>
2020-07-28 21:09:46 -0400

    completed task three

--- index.js
+++ index.js
@@ -65,8 +65,8 @@ For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainb
 
 function addFlavor(flavor, array){
 
-    array.unshift(flavor);
-    return array;
+    array.unshift(flavor);  //method adds item to beginning of array
+    return array;           //if unshift method is returned, it will just give length of array. You need to return array after unshift method. 
 
 }
 
@@ -86,11 +86,15 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
-function removeLastFlavor(/*code here*/){
+function removeLastFlavor(array) {
 
-    /*code here*/
+    array.pop(); //method operates on the end of an array. 
+
+    return array;
 
 }
+
+console.log(removeLastFlavor(originalFlavors));
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 

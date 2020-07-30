c3b207bf883b8b6d7d4e4c671afeaa52758768cd
Austin Greer <agreer.ag12@gmail.com>
2020-07-30 01:15:52 -0400

    austin-greer

--- index.js
+++ index.js
@@ -170,7 +170,9 @@ console.log(copy(copyFlavors, originalFlavors));
 
 
 
-/* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
+/* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the 
+array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke 
+with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
 Your function should accept: 
 
@@ -185,12 +187,23 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
-
+function filterByWord(array, string){
+    
+    let promoFlavors = []
+    
+    for (let i = 0; i < array.length; i++) {
+        
+        if (array[i].includes(string)) {
+            
+            promoFlavors.push(array[i]);
+        
+        } 
+    
+    }
+    return promoFlavors;
 }
 
+console.log(filterByWord(originalFlavors, "Chocolate"));
 
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
